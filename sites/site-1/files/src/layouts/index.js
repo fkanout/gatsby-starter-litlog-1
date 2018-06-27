b2f560960049286ac74d4bdb855d19d756725351
Carl W. Hunt <cwhunt@litlog.io>
2018-06-27 12:03:07 -0700

    added task-1 plan step-7

--- sites/site-1/files/src/layouts/index.js
+++ sites/site-1/files/src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
+import "prismjs/themes/prism-okaidia.css";
 import './index.css'
 
 const Layout = ({ children, data }) => (
