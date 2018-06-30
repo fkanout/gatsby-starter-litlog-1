532482cbb0f56e3432cc76b8c413e67fdd2e23a8
Carl W. Hunt <cwhunt@litlog.io>
2018-06-30 15:04:24 -0700

    added *to* prop

--- src/components/litlog-link.js
+++ src/components/litlog-link.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const LitlogLink = () => (
-  <span>
-    Hello, World!
-  </span>
-)
+const LitlogLink = ({ to }) => {
+  return (
+    <span>
+      {to}
+    </span>
+)}
 
 export default LitlogLink
