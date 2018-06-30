86415ff21bdfe87d8be0625fff7020758bd3d261
Carl W. Hunt <cwhunt@litlog.io>
2018-06-30 14:06:27 -0700

    added task-2 plan step-2

--- src/templates/litlog-page.js
+++ src/templates/litlog-page.js
@@ -1,11 +1,21 @@
 import React from "react";
+import rehypeReact from "rehype-react";
+import LitlogLink from "../components/litlog-link";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
+  const renderAst = new rehypeReact({
+    createElement: React.createElement,
+    components: { 
+      "litlog-link": LitlogLink
+    },
+  }).Compiler;
   return (
     <div>
       <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      {
+        renderAst(post.htmlAst)
+      }
     </div>
   );
 };
@@ -13,7 +23,7 @@ export default ({ data }) => {
 export const query = graphql`
   query LitlogPageQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
+      htmlAst
       frontmatter {
         title
       }
