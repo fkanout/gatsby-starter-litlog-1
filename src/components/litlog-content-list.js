d09900939219432c058dd1dd769cdb7601d6596f
Carl W. Hunt <cwhunt@litlog.io>
2018-06-30 16:59:53 -0700

    added task-3 plan step-3

--- src/components/litlog-content-list.js
+++ src/components/litlog-content-list.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react"
 import Link from "gatsby-link"
 
-const LitlogContentList = ({ type, data }) => {
-  return (
-    <table>
+const LwdContentList = ({ type, data }) => {
+  if (data) {
+    return (
+      <table>
       <thead>
         <tr>
           <th>link</th>
@@ -11,21 +12,31 @@ const LitlogContentList = ({ type, data }) => {
         </tr>
       </thead>
       <tbody>
-        {data.allMarkdownRemark.edges.map(({ node }, index) =>
-          <tr key={index}>
-            <td>
-            <Link to={node.fields.slug}>{node.fields.slug}</Link>
-            </td>
-            <td>
-              {node.frontmatter.title}
-            </td>
-          </tr>
-        )}
+        {data.allMarkdownRemark.edges.map(({ node }, index) => {
+          const slug = node.fields.slug.toString();
+          const fields = slug.split("/");
+          if (fields.length > 3 && fields[3] == type)
+          return (
+            <tr key={index}>
+              <td>
+              <Link to={node.fields.slug}>{node.fields.slug}</Link>
+              </td>
+              <td>
+                {node.frontmatter.title}
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </table>
-)}
+    )
+  } else {
+    return <span>no data</span>
+  }
+}
 
-export default LitlogContentList
+export default LwdContentList
+
 export const allMarkdownFragment = graphql`
 fragment AllMarkdownFragment on MarkdownRemarkConnection {
 	totalCount
