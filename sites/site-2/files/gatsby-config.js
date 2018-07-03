5d49d580658b78f3477ab19638f2f4c0cb7f0d9d
Carl W. Hunt <cwhunt@litlog.io>
2018-07-02 19:05:35 -0700

    added task-4 plan step-3

---
+++ sites/site-2/files/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Litlog Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `litlog`,
+        path: `${__dirname}/src/pages/litlog`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-prismjs`,
+        ]
+      }
+    }
+  ],
+  pathPrefix: '/gatsby-starter-litlog'
+}
